71a704153d6edbd97c564ea05b823afecf401b8f
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-08-18 16:55:16 +0100

    models

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const userSchema = new Schema({
+    reg:{
+        type:String,
+        required: [true, 'please provide you student registration id'],
+        unique: [true, "reg must be a unique field"],
+    },
+    fullname:{
+            type:String
+    },
+    department:{
+        type:String
+    },
+    password:{
+        type:String,
+        required: [true, 'Please provide password'],
+        minlength: 6,
+    },
+    accounttype:{
+        type: String,
+//         require: true
+//         default:"publice"
+    },
+    email:{
+        type:String,
+        unique: true,
+        required: [true, 'Please provide email'],
+        validate: {
+        validator: validator.isEmail,
+        message: 'Please provide valid email',
+    },
+    profilePicture:{
+        type:String
+    },
+    createdOn: { type: Date, default: Date.now },
+    modifiedOn: Date,
+    lastLogin: Date,
+    accountype:String
+},
+
+{
+    timestamps: true
+})
+module.exports = mongoose.model('User', userSchema)
